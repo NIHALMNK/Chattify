31b14785742c98663b64285ae0abe601b19674d1
mohammed nihal k <keedathnihal@gmail.com>
2025-10-28 15:39:23 +0530

    axios

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: "https://chattify-k2zj.onrender.com",
   withCredentials: true,
 });
